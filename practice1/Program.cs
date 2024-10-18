5e6c90f33837abd6c0ac3fd778daad96d1c0bfb0
HozyMei <nastusik-2005@mail.ru>
2024-10-18 23:36:05 +0700

    Add files via upload

---
+++ practice1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace practice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //MainConsole.PrintInfo();
+            Parsing parser = new Parsing();
+            string filePath = "C:\\Users\\toha3\\OneDrive\\Рабочий стол\\practice1\\practice1\\files.json";
+            List<FileDescription> files = parser.ParseJsonFile(filePath);
+            MainConsole.PrintInfo(files);
+        }
+    }
+}
